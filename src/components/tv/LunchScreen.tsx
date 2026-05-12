46968fc190dd460fe38b92bc3f96258ccb321fbe
Corentin Desjars <cod7k7@gmail.com>
2026-05-12 15:56:17 +0200

    feat: change photo carousel interval to 1 minute

--- src/components/tv/LunchScreen.tsx
+++ src/components/tv/LunchScreen.tsx
@@ -192,7 +192,7 @@ function PhotoCarousel({ photos }: { photos: Photo[] }) {
         setIdx(i => (i + 1) % photos.length)
         setVisible(true)
       }, 600)
-    }, 5000)
+    }, 60_000)
     return () => clearInterval(timer)
   }, [photos.length])
 
